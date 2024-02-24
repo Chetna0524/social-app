62b351a6653e45d829ac9b26dc61b8339e8d7351
Chetna0524 <chetnagupta2021@gmail.com>
2024-02-24 12:40:56 +0530

    added route for 404

--- src/App.js
+++ src/App.js
@@ -20,6 +20,8 @@ function App() {
 					<Route element={<PrivateRoute />}>
 						<Route path="/feeds" element={<Home />} />
 					</Route>
+
+					<Route path="*" element={<UserAuth />} />
 				</Route>
 			</Routes>
 		</>
