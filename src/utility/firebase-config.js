9ee95d340da82fc69fcf224ad2fc76401425cea7
Chetna0524 <159872600+Chetna0524@users.noreply.github.com>
2024-06-15 15:51:50 +0530

    Update firebase-config.js

--- src/utility/firebase-config.js
+++ src/utility/firebase-config.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-	apiKey: "AIzaSyB0ejrs_yh-fFfJdyKjcMBxBZq3m1uYuXg",
-	authDomain: "user-auth-a42cd.firebaseapp.com",
-	projectId: "user-auth-a42cd",
-	storageBucket: "user-auth-a42cd.appspot.com",
-	messagingSenderId: "821030931174",
-	appId: "1:821030931174:web:eccf7db47ed21bfd97c927",
+	apiKey: "XXX",
+	authDomain: "XXX",
+	projectId: "XXX",
+	storageBucket: "XXX",
+	messagingSenderId: "XXX4",
+	appId: "XXXX",
 };
 
 const app = initializeApp(firebaseConfig);
